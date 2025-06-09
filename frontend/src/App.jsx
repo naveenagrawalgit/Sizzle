015976f014d50d4a94b57ebd838c665cf9187e9b
naveen <naveenagrawal1008@gmail.com>
2025-06-09 23:24:10 +0530

    routes setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
 import Navbar from './componenet/Navbar'
 import { AuthProvider } from './context/AuthContext'
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import AddRecipe from './pages/AddRecipe'
 
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path="/register" element ={<Register/>} />
         <Route path="/login" element={<Login/>}/>
-        <Route/>
+        <Route path='/add-recipe' element={<AddRecipe/>} />
       </Routes>
 
 
